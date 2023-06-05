625023334d75b55567d39c673f6a7f756c18d681
chenh <chenh@ihep.ac.cn>
2023-06-06 00:32:43 +0800

    chchange

---
+++ build/CMakeFiles/exec.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/canmog/Desktop/g4work/Geant4Homework/main.cc" "CMakeFiles/exec.dir/main.cc.o" "gcc" "CMakeFiles/exec.dir/main.cc.o.d"
+  "/home/canmog/Desktop/g4work/Geant4Homework/src/B1ActionInitialization.cc" "CMakeFiles/exec.dir/src/B1ActionInitialization.cc.o" "gcc" "CMakeFiles/exec.dir/src/B1ActionInitialization.cc.o.d"
+  "/home/canmog/Desktop/g4work/Geant4Homework/src/B1Analysis.cc" "CMakeFiles/exec.dir/src/B1Analysis.cc.o" "gcc" "CMakeFiles/exec.dir/src/B1Analysis.cc.o.d"
+  "/home/canmog/Desktop/g4work/Geant4Homework/src/B1DetectorConstruction.cc" "CMakeFiles/exec.dir/src/B1DetectorConstruction.cc.o" "gcc" "CMakeFiles/exec.dir/src/B1DetectorConstruction.cc.o.d"
+  "/home/canmog/Desktop/g4work/Geant4Homework/src/B1EventAction.cc" "CMakeFiles/exec.dir/src/B1EventAction.cc.o" "gcc" "CMakeFiles/exec.dir/src/B1EventAction.cc.o.d"
+  "/home/canmog/Desktop/g4work/Geant4Homework/src/B1PrimaryGeneratorAction.cc" "CMakeFiles/exec.dir/src/B1PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/exec.dir/src/B1PrimaryGeneratorAction.cc.o.d"
+  "/home/canmog/Desktop/g4work/Geant4Homework/src/B1RunAction.cc" "CMakeFiles/exec.dir/src/B1RunAction.cc.o" "gcc" "CMakeFiles/exec.dir/src/B1RunAction.cc.o.d"
+  "/home/canmog/Desktop/g4work/Geant4Homework/src/B1SteppingAction.cc" "CMakeFiles/exec.dir/src/B1SteppingAction.cc.o" "gcc" "CMakeFiles/exec.dir/src/B1SteppingAction.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
